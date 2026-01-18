2423d63055a5bcb47ab41aeed8089d32ed59f909
Muhammad Huzaifa <muhammadhuzaifa_mh@yahoo.com>
2026-01-18 12:17:18 +0500

    Creater a user message model

---
+++ HRChatbotAPI/Models/ChatRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace HRChatbotAPI.Models
+{
+    public class ChatRequest
+    {
+        public int Id { get; set; }
+        public string? UserMessage { get; set; }
+
+        public DateTime TimeStamp { get; set; } = DateTime.Now;
+    }
+}
